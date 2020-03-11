deb6fabd340bd6a7eddb7f0ef57cf1ea827a93cf
Sydney Michael <m13s19madden@gmail.com>
2020-03-11 22:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
   // write your code here
   if (cart.length === 0) {
     return 'Your shopping cart is empty.'
-  } else if {
+  } else if (cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else {
     var st = 'In your cart, you have'
